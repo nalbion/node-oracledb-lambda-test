0c3758a47e355e387feebed24127c10636cd7a46
Nicholas Albion <nicholas.albion@justice.nsw.gov.au>
2016-07-06 18:23:05 +1000

    debug version

---
+++ index.js
@@ -0,0 +1,17 @@
+var oracledb = require('oracledb');
+/*
+exports.handler = function(event, context) {
+    console.info('event:', event);
+    console.info('context:', context);
+    console.info('oracledb:', oracledb);
+
+    if (oracledb != null) {
+        context.succeed('Oracle DB is supported');
+    } else {
+        context.fail('Did not actually expect to see this, `require(\'oracledb\')` will fail');
+    }
+};*/
+
+// module.exports = function() {
+    console.info('oracledb:', oracledb);
+// };
